37076ecdb65872d485c7153324773f9a85381212
Pietro Marino <36457039+pmarino84@users.noreply.github.com>
2024-10-08 16:38:32 +0200

    Feature/29 Improve reports data usage (#37)
    * feat: save metrics to csv files only if available
    
    * feat: draw metrics to pdf report only if available
    
    * feat: draw metrics to html report only if available

--- src/strategy_tester/metrics/save.py
+++ src/strategy_tester/metrics/save.py
@@ -35,35 +35,59 @@ def save_metrics(entries_bars_by_hour: pd.DataFrame, entries_bars_by_dow: pd.Dat
   `file_suffix` optional file suffix to customize the file name.\n
   """
   file_suffix = _create_file_suffix(file_suffix)
-  entries_bars_by_hour_filename = f"{parent_folder}/entries_by_hour{file_suffix}.csv"
-  entries_bars_by_dow_filename = f"{parent_folder}/entries_by_dow{file_suffix}.csv"
-  entries_bars_by_month_filename = f"{parent_folder}/entries_by_month{file_suffix}.csv"
-  entries_bars_by_hour.to_csv(entries_bars_by_hour_filename)
-  entries_bars_by_dow.to_csv(entries_bars_by_dow_filename)
-  entries_bars_by_month.to_csv(entries_bars_by_month_filename)
 
-  profits_losses_by_hour_filename = f"{parent_folder}/profits_losses_by_hour{file_suffix}.csv"
-  profits_losses_by_dow_filename = f"{parent_folder}/profits_losses_by_dow{file_suffix}.csv"
-  profits_losses_by_month_filename = f"{parent_folder}/profits_losses_by_month{file_suffix}.csv"
-  profits_losses_by_hour.to_csv(profits_losses_by_hour_filename)
-  profits_losses_by_dow.to_csv(profits_losses_by_dow_filename)
-  profits_losses_by_month.to_csv(profits_losses_by_month_filename)
+  if not entries_bars_by_hour.empty:
+    entries_bars_by_hour_filename = f"{parent_folder}/entries_by_hour{file_suffix}.csv"
+    entries_bars_by_hour.to_csv(entries_bars_by_hour_filename)
+  
+  if not entries_bars_by_dow.empty:
+    entries_bars_by_dow_filename = f"{parent_folder}/entries_by_dow{file_suffix}.csv"
+    entries_bars_by_dow.to_csv(entries_bars_by_dow_filename)
+  
+  if not entries_bars_by_month.empty:
+    entries_bars_by_month_filename = f"{parent_folder}/entries_by_month{file_suffix}.csv"
+    entries_bars_by_month.to_csv(entries_bars_by_month_filename)
 
-  profits_losses_mean_by_hour_filename = f"{parent_folder}/profits_losses_mean_by_hour{file_suffix}.csv"
-  profits_losses_mean_by_dow_filename = f"{parent_folder}/profits_losses_mean_by_dow{file_suffix}.csv"
-  profits_losses_mean_by_month_filename = f"{parent_folder}/profits_losses_mean_by_month{file_suffix}.csv"
-  pd.DataFrame(profits_losses_mean_by_hour).to_csv(profits_losses_mean_by_hour_filename)
-  pd.DataFrame(profits_losses_mean_by_dow).to_csv(profits_losses_mean_by_dow_filename)
-  pd.DataFrame(profits_losses_mean_by_month).to_csv(profits_losses_mean_by_month_filename)
+  if not profits_losses_by_hour.empty:
+    profits_losses_by_hour_filename = f"{parent_folder}/profits_losses_by_hour{file_suffix}.csv"
+    profits_losses_by_hour.to_csv(profits_losses_by_hour_filename)
 
-  profits_losses_sum_by_hour_filename = f"{parent_folder}/profits_losses_sum_by_hour{file_suffix}.csv"
-  profits_losses_sum_by_dow_filename = f"{parent_folder}/profits_losses_sum_by_dow{file_suffix}.csv"
-  profits_losses_sum_by_month_filename = f"{parent_folder}/profits_losses_sum_by_month{file_suffix}.csv"
-  pd.DataFrame(profits_losses_sum_by_hour).to_csv(profits_losses_sum_by_hour_filename)
-  pd.DataFrame(profits_losses_sum_by_dow).to_csv(profits_losses_sum_by_dow_filename)
-  pd.DataFrame(profits_losses_sum_by_month).to_csv(profits_losses_sum_by_month_filename)
+  if not profits_losses_by_dow.empty:
+    profits_losses_by_dow_filename = f"{parent_folder}/profits_losses_by_dow{file_suffix}.csv"
+    profits_losses_by_dow.to_csv(profits_losses_by_dow_filename)
+  
+  if not profits_losses_by_month.empty:
+    profits_losses_by_month_filename = f"{parent_folder}/profits_losses_by_month{file_suffix}.csv"
+    profits_losses_by_month.to_csv(profits_losses_by_month_filename)
 
-  profits_by_time_opened_filename = f"{parent_folder}/profits_by_time_opened{file_suffix}.csv"
-  losses_by_time_opened_filename = f"{parent_folder}/losses_by_time_opened{file_suffix}.csv"
-  profits_by_time_opened.to_csv(profits_by_time_opened_filename)
-  losses_by_time_opened.to_csv(losses_by_time_opened_filename)
+  if not profits_losses_mean_by_hour.empty:
+    profits_losses_mean_by_hour_filename = f"{parent_folder}/profits_losses_mean_by_hour{file_suffix}.csv"
+    pd.DataFrame(profits_losses_mean_by_hour).to_csv(profits_losses_mean_by_hour_filename)
+
+  if not profits_losses_mean_by_dow.empty:
+    profits_losses_mean_by_dow_filename = f"{parent_folder}/profits_losses_mean_by_dow{file_suffix}.csv"
+    pd.DataFrame(profits_losses_mean_by_dow).to_csv(profits_losses_mean_by_dow_filename)
+  
+  if not profits_losses_mean_by_month.empty:
+    profits_losses_mean_by_month_filename = f"{parent_folder}/profits_losses_mean_by_month{file_suffix}.csv"
+    pd.DataFrame(profits_losses_mean_by_month).to_csv(profits_losses_mean_by_month_filename)
+
+  if not profits_losses_sum_by_hour.empty:
+    profits_losses_sum_by_hour_filename = f"{parent_folder}/profits_losses_sum_by_hour{file_suffix}.csv"
+    pd.DataFrame(profits_losses_sum_by_hour).to_csv(profits_losses_sum_by_hour_filename)
+  
+  if not profits_losses_sum_by_dow.empty:
+    profits_losses_sum_by_dow_filename = f"{parent_folder}/profits_losses_sum_by_dow{file_suffix}.csv"
+    pd.DataFrame(profits_losses_sum_by_dow).to_csv(profits_losses_sum_by_dow_filename)
+  
+  if not profits_losses_sum_by_month.empty:
+    profits_losses_sum_by_month_filename = f"{parent_folder}/profits_losses_sum_by_month{file_suffix}.csv"
+    pd.DataFrame(profits_losses_sum_by_month).to_csv(profits_losses_sum_by_month_filename)
+
+  if not profits_by_time_opened.empty:
+    profits_by_time_opened_filename = f"{parent_folder}/profits_by_time_opened{file_suffix}.csv"
+    profits_by_time_opened.to_csv(profits_by_time_opened_filename)
+  
+  if not losses_by_time_opened.empty:
+    losses_by_time_opened_filename = f"{parent_folder}/losses_by_time_opened{file_suffix}.csv"
+    losses_by_time_opened.to_csv(losses_by_time_opened_filename)
