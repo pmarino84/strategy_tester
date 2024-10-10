01fdfe97a9d6c1d165914911cab645d06f2b49b4
Pietro Marino <36457039+pmarino84@users.noreply.github.com>
2024-10-10 09:26:28 +0200

    feat: Add profits_losses_mean_by_hour/dow/month charts to plot_metrics (#51)

--- src/strategy_tester/plot/plot_metrics.py
+++ src/strategy_tester/plot/plot_metrics.py
@@ -4,7 +4,6 @@ import pandas as pd
 from ..pipeline.context import Context
 
 
-# TODO: aggiungere profits_losses_mean_by_hour/dow/month
 def plot_metrics(context: Context):
   """
   Plot statistics, equity and metrics with the help of `matplotlib`.
@@ -33,6 +32,10 @@ def plot_metrics(context: Context):
   profits_losses_sum_by_dow = metrics["profits_losses_sum_by_dow"] if "profits_losses_sum_by_dow" in metrics else pd.DataFrame()
   profits_losses_sum_by_month = metrics["profits_losses_sum_by_month"] if "profits_losses_sum_by_month" in metrics else pd.DataFrame()
 
+  profits_losses_mean_by_hour = metrics["profits_losses_mean_by_hour"] if "profits_losses_mean_by_hour" in metrics else pd.DataFrame()
+  profits_losses_mean_by_dow = metrics["profits_losses_mean_by_dow"] if "profits_losses_mean_by_dow" in metrics else pd.DataFrame()
+  profits_losses_mean_by_month = metrics["profits_losses_mean_by_month"] if "profits_losses_mean_by_month" in metrics else pd.DataFrame()
+
   profits_losses_by_hour = metrics["profits_losses_by_hour"] if "profits_losses_by_hour" in metrics else pd.DataFrame()
   profits_losses_by_dow = metrics["profits_losses_by_dow"] if "profits_losses_by_dow" in metrics else pd.DataFrame()
   profits_losses_by_month = metrics["profits_losses_by_month"] if "profits_losses_by_month" in metrics else pd.DataFrame()
@@ -40,10 +43,6 @@ def plot_metrics(context: Context):
   entries_by_hour = metrics["entries_by_hour"] if "entries_by_hour" in metrics else pd.DataFrame()
   entries_by_dow = metrics["entries_by_dow"] if "entries_by_dow" in metrics else pd.DataFrame()
   entries_by_month = metrics["entries_by_month"] if "entries_by_month" in metrics else pd.DataFrame()
-
-  # profits_losses_mean_by_hour = metrics["profits_losses_mean_by_hour"] if "profits_losses_mean_by_hour" in metrics else pd.DataFrame()
-  # profits_losses_mean_by_dow = metrics["profits_losses_mean_by_dow"] if "profits_losses_mean_by_dow" in metrics else pd.DataFrame()
-  # profits_losses_mean_by_month = metrics["profits_losses_mean_by_month"] if "profits_losses_mean_by_month" in metrics else pd.DataFrame()
 
   profits_by_time_opened = metrics["profits_by_time_opened"] if "profits_by_time_opened" in metrics else pd.DataFrame()
   losses_by_time_opened = metrics["losses_by_time_opened"] if "losses_by_time_opened" in metrics else pd.DataFrame()
@@ -106,6 +105,27 @@ def plot_metrics(context: Context):
     profits_losses_sum_by_month_colors = profits_losses_sum_by_month.map(lambda x: "green" if x > 0 else "red").to_list()
     profits_losses_sum_by_month.plot.bar(ax=axes["I"], color=profits_losses_sum_by_month_colors)
     axes["I"].set_title("Profits/Losses Sum by Month")
+    axes["I"].legend().remove()
+    axes["I"].set_xlabel("")
+
+  if not profits_losses_mean_by_hour.empty:
+    profits_losses_mean_by_hour_colors = profits_losses_mean_by_hour.map(lambda x: "green" if x > 0 else "red").to_list()
+    profits_losses_mean_by_hour.plot.bar(ax=axes["G"], color=profits_losses_mean_by_hour_colors)
+    axes["G"].set_title("Profits/Losses Mean by Hour")
+    axes["G"].legend().remove()
+    axes["G"].set_xlabel("")
+
+  if not profits_losses_mean_by_dow.empty:
+    profits_losses_mean_by_dow_colors = profits_losses_mean_by_dow.map(lambda x: "green" if x > 0 else "red").to_list()
+    profits_losses_mean_by_dow.plot.bar(ax=axes["H"], color=profits_losses_mean_by_dow_colors)
+    axes["H"].set_title("Profits/Losses Mean by Day Of Week")
+    axes["H"].legend().remove()
+    axes["H"].set_xlabel("")
+
+  if not profits_losses_mean_by_month.empty:
+    profits_losses_mean_by_month_colors = profits_losses_mean_by_month.map(lambda x: "green" if x > 0 else "red").to_list()
+    profits_losses_mean_by_month.plot.bar(ax=axes["I"], color=profits_losses_mean_by_month_colors)
+    axes["I"].set_title("Profits/Losses Mean by Month")
     axes["I"].legend().remove()
     axes["I"].set_xlabel("")
 
